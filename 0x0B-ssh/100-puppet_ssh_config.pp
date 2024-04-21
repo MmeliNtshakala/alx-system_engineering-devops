b66c4989bfa692c1d2963c55b58b6eff89ebabd2
MmeliNtshakala <mmeliroot@gmail.com>
2024-04-21 11:19:18 -0700

    puppet ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,17 @@
-#!/usr/bin/env bash
-# using  puppet to change ile config
-
-file { "ect/ssh/ssh_config":
-	ensure => present,
-content =>"
-
-	#SSH client configuration
-	host*
-	IdentityFile /.ssh/school
-	PasswordAuthentication no
-	"
-
+# Your SSH client configuration must be configured to use the private key ~/.ssh/school
+# Your SSH client configuration must be configured to refuse to authenticate using a password
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+#
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
